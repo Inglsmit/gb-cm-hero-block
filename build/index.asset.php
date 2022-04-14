c6052e5435898dd0702a2c3dd8ed7910703ef2b6
Inglsmit <paul.inglsmit@gmail.com>
2022-04-14 16:54:55 +0300

    upd: build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7150db0ed9db839daee4ecb75e847d16');
+<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd173d8b61d1ac0b5858e5e5a6200fbdc');
